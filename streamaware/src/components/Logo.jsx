e4fac61c5b8a788a01c922184960e6a18da859b5
MiaAgnes <eaa24matd@students.eaaa.dk>
2025-10-24 12:20:08 +0200

    homepage

--- streamaware/src/components/Logo.jsx
+++ streamaware/src/components/Logo.jsx
@@ -1,4 +1,10 @@
 // Logo component
 export default function Logo() {
-  return <div>Logo</div>;
+  return (
+    <div>
+      <a href="/homepage">
+        <img src="/logo.png" alt="StreamAware Logo" width="150" />
+      </a>
+    </div>
+  );
 }
--- streamaware/src/pages/Homepage.jsx
+++ streamaware/src/pages/Homepage.jsx
@@ -1,10 +1,10 @@
 import styles from './Homepage.module.css';
+import Logo from '../components/Logo.jsx';
 
 export default function Homepage() {
   return (
     <div className={styles.container}>
-      <h1>Homepage</h1>
-      <p>Homepage kommer her</p>
+      <Logo />
     </div>
   );
 }
