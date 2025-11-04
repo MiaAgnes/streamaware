ccde2473ed93c167441ee9c4b1200650639b8569
eaa24martia <eaa24martia@gmail.com>
2025-11-04 15:13:52 +0100

    changed vite.config.js

--- streamaware/vite.config.js
+++ streamaware/vite.config.js
@@ -2,10 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true, // Expose to network
-    port: 5173,
-  },
-})
+export default defineConfig(({ command }) => {
+  const isServe = command === "serve";
+
+  const config = {
+    plugins: [react()],
+    server: {
+      host: true, // Expose to network
+      port: 5173,
+    },
+    base: "/",
+  };
+
+  // Change base path when building for production
+  if (!isServe) {
+    config.base = "/streamaware/"; // 👈 Replace with your GitHub repository name
+  }
+
+  return config;
+});
